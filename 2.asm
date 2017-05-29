4b115060e2c638470e2ee77cc7f15e1120a735be
fireflygy <1332420858@qq.com>
2017-05-29 10:48:08 +0800

    2.asm
    查找字符串中是否有s字符

---
+++ 2.asm
@@ -0,0 +1,33 @@
+data segment
+
+string2  db 'FOUND$'
+string3  db'NOT FOUND$'
+data ends
+cout_string  db 20 dup(?)
+extra segment
+string1 db 'a string for testing$'
+extra ends
+
+assume ds:data,es:extra,cs:code
+code segment
+start:
+       mov ax,data
+	   mov ds,ax
+	   mov ax,extra
+	   mov es,ax
+	   mov di,offset string1
+	   cld
+	   mov cx,21
+	   mov al,'s'
+	   repnz scasb
+	   jnz notfound	
+       mov dx,offset string2
+       jmp exit	   
+notfound:  
+       mov dx,offset string3 
+exit:  mov ah,9
+	   int 21h
+	   mov ah,4ch
+	   int 21h
+code ends
+end start
